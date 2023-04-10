981cd2745ab224ed879e40a0f436ab5f8a917021
vikramw3dev <vikram.i@w3dev.email>
2023-04-10 14:26:47 +0530

    Added logic for game, some fix needed for solved images

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,56 +1,93 @@
 import React, { useEffect, useRef, useState } from 'react'
-import '../css/box.css'
+import style from '../css/box.module.css'
 import { images } from './Images';
 export default function Box() {
     const renderCount = useRef(0);
     const [name, setname] = useState(true)
-    const [allimages, setAllImages] = useState(images.concat(images))
+    const [allimages, setAllImages] = useState(images)
     const [activeImage, setActiveImage] = useState([])
+    const [unactiveImages, setUnactiveImages] = useState([])
 
-    const similar = () => {
+    const similar = (image) => {
+        if(activeImage){
+            let [img1, img2] = activeImage 
+            if(img1!==undefined && img2!==undefined)
+            if (img1?.title==img2?.title){
+                console.log('called simi', img1.index, img2.index)
+                
+                // Removing the image from img element and adding solved style to parent element
+                document.getElementById(img1.title+img1.index).classList.add(style.hide)
+                document.getElementById(img1.title+img1.index+'_c').classList.add(style.solved)
+                document.getElementById(img2.title+img2.index).classList.add(style.hide)
+                document.getElementById(img2.title+img2.index+'_c').classList.add(style.solved)
 
+                // Setting the images which are solved to true, it will be used before flipping os images
+                let img_object1 = allimages.findIndex((image => image.title == img1.title));
+                allimages[img_object1].solved = true
+                console.log('fff: ',img_object1,allimages[img_object1])
+                let img_object2 = allimages.findIndex((image => image.title == img2.title)); // Jan's method: a.indexOf(99, a.indexOf(99) + 1)
+                allimages[img_object2].solved = true
+                console.log('fff: ',img_object2,allimages[img_object2])
+            }
+            
+        }
     }
 
-    const unflip = (img_index) =>{
-        console.log('called unflip')
-            images.forEach(function(image, index){
-                document.getElementById(image.title+img_index).classList.add('hide')
-            })
+    const unflip = (img_index) => {
+        images.forEach(function (image, index) {
+            // console.log('called unflip', image.title+index)
+            document.getElementById(image.title + index).classList.add(style.hide)
+            image.display = false
+            activeImage.pop()
+        })
     }
     // count number of renders
-    useEffect(()=>{
+    useEffect(() => {
         renderCount.current = renderCount.current + 1
 
         // timeout for flip
         const timer = setTimeout(() => {
-          }, 2000);
-          return () => clearTimeout(timer);
+        }, 2000);
+        return () => clearTimeout(timer);
 
     })
+    useEffect(() => {
+        if (activeImage.length === 2) {
+          setTimeout(similar, 500);
+        }
+      }, [activeImage]);
+
     const flip = (image, index) => {
-        if ((renderCount.current+1)%2==0){
+        // console.log('index: ', index)
+        if ((renderCount.current + 1) % 2 == 0 ) {
+            // similar()
             unflip(index)
         }
-        if (!image.display){
-            let id = image.title+index
-            console.log('called flip', id)
-            document.getElementById(id).classList.remove('hide')
+        if ((renderCount.current) % 2 == 0) {
+            similar(image)
+            // unflip(index)
+        }
+        if (!image.display && !image.solved) {
+            let id = image.title + index
+            let [img1, img2] = activeImage 
+            // console.log('called flip', id)
+            document.getElementById(id).classList.remove(style.hide)
             image.display = true
-            setname(!name)
+            setActiveImage([...activeImage,{'title':image.title,'index':index}])
         }
     }
-    console.log('count: ', renderCount.current)
 
     return (
-    <div className='container'>
-        <div className='grid-container'>
-            {allimages.map((image, index)=>{
-                return <div className='grid-item' onClick={()=>flip(image, index)}>
-                    <img src={image.location} className='hide' id={image.title+index}></img>
-                </div>
-            })}
+        <div className={style.container} style={{backgroundColor:'#f4dcdc'}}>
+            <div className={style.play_score}> Move: {renderCount.current}</div>
+            <div className={style.grid_container}>
+                {allimages.map((image, index) => {
+                    return <div id={image.title+index+'_c'} className={style.grid_item} onClick={() => flip(image, index)}>
+                        <img src={image.location} className={style.hide} id={image.title+index}></img>
+                    </div>
+                })}
+            </div>
         </div>
-    </div>
-    
-  )
+
+    )
 }
--- src/components/Images.js
+++ src/components/Images.js
@@ -2,42 +2,99 @@ export const images = [
     {
         title: 'tajmahal',
         location: require('../assets/tajmahal.jpg'),
-        display: false
+        display: false,
+        solved: false
     },
     {
         title: 'chichen',
         location: require('../assets/chichen.webp'),
-        display: false
+        display: false,
+        solved: false
     }
     ,
     {
         title: 'christ',
         location: require('../assets/christ.webp'),
-        display: false
+        display: false,
+        solved: false
     },
     {
         title: 'colossuem',
         location: require('../assets/colossuem.jpg'),
-        display: false
+        display: false,
+        solved: false
     },
     {
         title: 'greatwall',
         location: require('../assets/greatwall.jpg'),
-        display: false
+        display: false,
+        solved: false
     },
     {
         title: 'peru',
         location: require('../assets/peru.jpg'),
-        display: false
+        display: false,
+        solved: false
     },
     {
         title: 'petra',
         location: require('../assets/petra.avif'),
-        display: false
+        display: false,
+        solved: false
     },
     {
         title: 'pyramid',
         location: require('../assets/pyramids.avif'),
-        display: false
+        display: false,
+        solved: false
+    },
+    {
+        title: 'tajmahal',
+        location: require('../assets/tajmahal.jpg'),
+        display: false,
+        solved: false
+    },
+    {
+        title: 'chichen',
+        location: require('../assets/chichen.webp'),
+        display: false,
+        solved: false
+    }
+    ,
+    {
+        title: 'christ',
+        location: require('../assets/christ.webp'),
+        display: false,
+        solved: false
+    },
+    {
+        title: 'colossuem',
+        location: require('../assets/colossuem.jpg'),
+        display: false,
+        solved: false
+    },
+    {
+        title: 'greatwall',
+        location: require('../assets/greatwall.jpg'),
+        display: false,
+        solved: false
+    },
+    {
+        title: 'peru',
+        location: require('../assets/peru.jpg'),
+        display: false,
+        solved: false
+    },
+    {
+        title: 'petra',
+        location: require('../assets/petra.avif'),
+        display: false,
+        solved: false
+    },
+    {
+        title: 'pyramid',
+        location: require('../assets/pyramids.avif'),
+        display: false,
+        solved: false
     }
 ]
