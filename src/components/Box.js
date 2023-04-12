1707dcf7c1c9c332241c05a243863b9fd8b7cc08
Vikram Chowdhury <vikram@themetromaxgroup.com>
2023-04-12 18:16:11 +0530

    Working!

--- src/components/Box.js
+++ src/components/Box.js
@@ -7,39 +7,113 @@ export default function Box() {
     const [cardSelected1, setCardSelected1] = useState(null)
     const [cardSelected2, setCardSelected2] = useState(null)
     const [moves, setMoves] = useState(0);
-    const [seconds, setSeconds] = useState(0);
+    const [disable, setDisabled] = useState(false);
     const [timer, setTimer] = useState(null);
+    const [seconds, setSeconds] = useState(0)
+    const [win, setWin] = useState(false)
 
-    // console.log('img: ',allimages)
+    // Count Time
+    useEffect(() => {
+        if(moves===1){
+            setTimer(setInterval(() => {
+                setSeconds(i => i + 1)
+            }, 1000))
+        }
+        // call the win method to check win condition
+        checkWin()
+    }, [moves]);
+    
+    //WiN Condition
+    const checkWin = () => {
+        let count = 0;
+        allimages.forEach((image)=>{
+            if(image.solved){
+                count=count+1
+            }
+        if(count===16){
+            setWin(true)
+            stopTime()
+        }
+        })
+    }
+
+    // Method for stoping timer
+    const stopTime = () => {
+        clearInterval(timer)
+        setTimer(null)
+    }
 
     const startGame = () => {
-        const randomize = [...images, ...images].sort(()=>Math.random() -0.5).
-            map((cards)=>({...cards,id:Math.random()}))
+        const randomize = [...images, ...images].sort(() => Math.random() - 0.5).
+            map((cards) => ({ ...cards, id: Math.random() }))
 
         setAllImages(randomize);
         setCardSelected1(null);
         setCardSelected2(null)
+        setTimer(null);
+        setMoves(0);
+        setSeconds(0);
+        setWin(false);
+        setDisabled(false)
+        stopTime()
     }
+    // compare 2 selected cards
+    useEffect(() => {
+        if (cardSelected1 || cardSelected2){
+            setMoves(preMoves=>preMoves + 1)
+        }
+        if (cardSelected1 && cardSelected2) {
+            // Counting the User moves ie every click.
+            // Disabling the clicking of Images until two images are evaluated
+            setDisabled(true)
+            if (cardSelected1.location === cardSelected2.location) {
+                console.log('inside card compare')
 
-    useEffect(()=>{
+                setAllImages(prevallimages => {
+                    return prevallimages.map(image => {
+                        if (image.location === cardSelected1.location) {
+                            return { ...image, solved: true }
+                        } else {
+                            return image
+                        }
+                    })
+                })
+                nextTurn()
+            } else {
+                setTimeout(() => nextTurn(), 1000)
+            }
+
+        }
+    }, [cardSelected1, cardSelected2])
+
+    const nextTurn = () => {
+        // setting the two selected images as null and enabling clicking of images
+        setCardSelected1(null);
+        setCardSelected2(null);
+        setDisabled(false)
+    }
+    useEffect(() => {
         startGame()
-    },[])
+    }, [])
 
     const handleCardClick = (card) => {
-        console.log('card: ',card)
-        if(cardSelected1 && cardSelected2){
-            cardSelected1? setCardSelected2(card) : setCardSelected1(card);
-            setMoves(moves=>moves+1)
-        }
+
+        cardSelected1 ? setCardSelected2(card) : setCardSelected1(card);
+        // setMoves(moves=>moves+1)
+        console.log('card selected: ', card)
     }
     return (
-        <div>
+        <div className={style.container}>
+            {!win && <div className={style.moves}>Moves: {moves} Time: {seconds} seconds</div>}
+            {win && <div className={style.moves}>Congrats!! You won. Moves: {moves} Time: {seconds} seconds</div>}
+            {win && <div className={style.moves} onClick={startGame}>Play again</div>}
             <div className={style.grid_container}>
-                {allimages.map((image)=>{
+                {allimages.map((image, index) => {
                     return <div>
-                        <Card className card={image} 
-                            handleClick={handleCardClick} 
-                            flip={image==cardSelected1 || image==cardSelected2} ></Card>
+                        <Card key={image.id} className card={image}
+                            handleClick={handleCardClick}
+                            disable={disable}
+                            turn={image === cardSelected1 || image === cardSelected2 || image.solved} ></Card>
                     </div>
                 })}
             </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import style from '../css/box.module.css'
 
-export default function Card({card, handleClick, flip}) {
-    console.log('flip: ',flip)
+export default function Card({card, handleClick, turn, disable}) {
+    const Click = () => {
+        if(!disable){
+            handleClick(card)
+        }
+    }
   return (
-        <div className={style.card}>
-            <div className={flip? style.flip:style.flip}>
-                <img className={style.image} src={card.location}></img>
-                <img className={style.background} src={require('../assets/solved.jpg')} onClick={()=>handleClick(card)}></img>
+
+    <div className={style.card}>    
+            <div className={turn ? style.turn : ""}>
+                <img className={style.image} src={card.location} alt="card front" />
+                <img className={style.background} src={require('../assets/solved.jpg')} onClick={Click} alt="cover" />
             </div>
         </div>
   )
